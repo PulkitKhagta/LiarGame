50014fcd5d88a48ff9990a14ef682cf523dc7524
PulkitKhagta <khagtapulkit@gmail.com>
2019-08-28 22:58:24 +0530

    PlayerMovement and health and Shooting Sysytem implemented

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int  health = 10;
+    public float speed;
+    public Text healthDisplay;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 moveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        healthDisplay.text = "Health : " + health;
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if(moveInput.x == 0 && moveInput.y == 0)
+        {
+            anim.SetBool("IsWalking", false);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", true);
+        }
+
+        /*if(Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetBool("HasWon", true);
+        }
+        else
+        {
+            anim.SetBool("HasWon", false);
+        }*/
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/ShootAndAim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAndAim : MonoBehaviour
+{
+    public GameObject shot;
+    private Transform GunPos;
+
+    private void Start()
+    {
+        GunPos = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        faceCursor();
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(shot, GunPos.position, Quaternion.identity);
+        }
+    }
+
+    void faceCursor()
+    {
+        Vector2 CursorPos = Input.mousePosition;
+        CursorPos = Camera.main.ScreenToWorldPoint(CursorPos);
+
+        Vector2 Direction = new Vector2(
+            CursorPos.x - transform.position.x,
+            CursorPos.y - transform.position.y
+            );
+
+        transform.right = Direction;
+        
+    }
+}
